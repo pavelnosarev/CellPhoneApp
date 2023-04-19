27a19041a1d5132d99b09b78b427a323f700d57a
pavelnosarev <pavelnosarev@outlook.com>
2023-04-19 11:50:12 -0400

    testing

---
+++ src/main/java/com/yearup/CellPhoneApp.java
@@ -0,0 +1,108 @@
+package com.yearup;
+
+
+
+
+
+
+
+public class CellPhoneApp {
+
+    public static void main (String[] args ) {
+        CellPhone iphone  = new CellPhone(123456789, "iPhone 12 Pro Max", "T-Mobile", 916-759-2872, "Pavel", 100);
+
+        iphone.goOnFaceTime();
+        iphone.goOnACall();
+        System.out.println(iphone.getBattery());
+        iphone.chargeBattery();
+    }
+}
+
+class CellPhone{
+    //properties//
+    private long  serialNumber;
+    private String model;
+    private String carrier;
+    private long phoneNumber;
+    private String owner;
+    private int battery;
+
+    ///getter and setters//
+    public CellPhone(long serialNumber, String model, String carrier,
+                     long phoneNumber, String owner, int battery) {
+                this.serialNumber = serialNumber;
+                this.model = model;
+                this. carrier = carrier;
+                this. phoneNumber = phoneNumber;
+                this.owner = owner;
+                this.battery = battery;
+
+    }
+    public long  getSerialNumber(){
+
+        return this.serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public void setSerialNumber(long serialNumber) {
+        this.serialNumber = serialNumber;
+
+    }
+
+    public void goOnACall(){
+        this.battery -= 15;
+
+
+    }
+    public void  goOnFaceTime(){
+        this.battery -= 20;
+    }
+
+    public int getBattery() {
+        return battery;
+    }
+
+    public void setBattery(int battery) {
+        this.battery = battery;
+    }
+
+    public void  chargeBattery(){
+         while (this.battery<101 ) {
+             System.out.println(this.battery);
+             int i = this.battery++;
+         }
+    }
+}
+
+
